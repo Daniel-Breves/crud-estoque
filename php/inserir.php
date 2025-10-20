b7c2b796a97c7908e07600f4171621ea38706f4d
DANIEL BREVES NUNES <danielbrevesnunes@gmail.com>
2025-10-20 15:28:30 -0300

    apenas inserindo os produtos, bem no começo do projeto

---
+++ php/inserir.php
@@ -0,0 +1,40 @@
+<?php
+//conecta ao db e testa conexão
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$db = "estoque_db";
+
+$conexao = new mysqli ($servidor, $usuario, $senha, $db);
+
+if($conexao->connect_error){
+        die("error de conexao:" .$conexao->connect_error);
+    }
+
+//pegando do formulario e jogando no banco
+
+$nom = $_POST["nome"];
+$desc = $_POST["descri"];
+$quant = $_POST["quanti"];
+$preco = $_POST["price"];
+
+$sql = "INSERT INTO produtos (nome, descricao, quantidade, preco)
+        VALUES (?,?,?,?)";
+
+//traz segurança evitando erros
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("ssis",$nom, $desc, $quant, $preco);
+
+
+if($stmt->execute()){
+    echo "<script>alert('Livro cadastrado com sucesso!');</script>";
+       
+    } else {
+        echo "<script>alert('Erro ao inserir livro: " . $stmt->error . "');</script>";
+    }
+    
+
+    $stmt->close();
+    $conexao->close();
+
+?>
